012350972b90084d93e3ad9262f84b2df4848c6c
mattd01 <matthewdelgado08@gmail.com>
2022-09-25 13:13:10 -0500

    UPDATED css character and obstacle creation

--- index.js
+++ index.js
@@ -1,112 +1,106 @@
-const game = document.getElementById('canvas')
-const obs = document.getElementById('obstacle')
-const ctx = game.getContext('2d')
+// const game = document.getElementById('canvas')
+// const ctx = game.getContext('2d')
 
-game.setAttribute('width', getComputedStyle(game)['width'])
-game.setAttribute('height', getComputedStyle(game)['height'])
+// game.setAttribute('width', getComputedStyle(game)['width'])
+// game.setAttribute('height', getComputedStyle(game)['height'])
 
-const gravity = 0.5
+// const gravity = 0.5
 
-// player creation
-class Player {
-    constructor() {
-        this.position = {
-            x: 20,
-            y: 180
-        }
-        this.velocity = {
-            x: 0,
-            y: 1
-        }
-        this.width = 50
-        this.height = 80
-    }
-    build() {
-        ctx.fillStyle = 'red'
-        ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
-    }
-    update() {
-        this.build()
-        this.position.y += this.velocity.y
-        this.position.x += this.velocity.x
-        if (this.position.y +this.height + this.velocity.y <= canvas.height)
-        this.velocity.y += gravity
-        else this.velocity.y = 0
-    }
-}
+// // player creation
+// class Player {
+//     constructor() {
+//         this.position = {
+//             x: 20,
+//             y: 180
+//         }
+//         this.velocity = {
+//             x: 0,
+//             y: 1
+//         }
+//         this.width = 50
+//         this.height = 80
+//     }
+//     build() {
+//         ctx.fillStyle = 'red'
+//         ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
+//     }
+//     update() {
+//         this.build()
+//         this.position.y += this.velocity.y
+//         this.position.x += this.velocity.x
+//         if (this.position.y +this.height + this.velocity.y <= canvas.height)
+//         this.velocity.y += gravity
+//         else this.velocity.y = 0
+//     }
+// }
 
-const player = new Player()
+// const player = new Player()
 
-// gravity and player movement
-function animate() {
-    requestAnimationFrame(animate)
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-    player.update()
-}
-animate()
+// // gravity and player movement
+// function animate() {
+//     requestAnimationFrame(animate)
+//     ctx.clearRect(0, 0, canvas.width, canvas.height)
+//     player.update()
+// }
+// animate()
 
-addEventListener('keydown', ({keyCode}) => {
-    switch (keyCode) {
-        case 65:
-        case 37:
-            console.log('left')
-            player.velocity.x -= 20
-            break
+// addEventListener('keydown', ({keyCode}) => {
+//     switch (keyCode) {
+//         case 65:
+//         case 37:
+//             console.log('left')
+//             player.velocity.x -= 5
+//             break
          
-        case 83:
-        case 40:
-            console.log('down')
-            player.velocity.y += 15
-            break
-        case 68:
-        case 39:
-            console.log('right')
-            player.velocity.x += 10
-            break   
-        case 87:
-        case 38:
-            console.log('up')
-            player.velocity.y -= 25
-            break
-    }
-})
+//         case 83:
+//         case 40:
+//             console.log('down')
+//             player.velocity.y += 15
+//             break
+//         case 68:
+//         case 39:
+//             console.log('right')
+//             player.velocity.x += 5
+//             break   
+//         case 87:
+//         case 38:
+//             console.log('up')
+//             player.velocity.y -= 20
+//             break
+//     }
+// })
 
-addEventListener('keyup', ({keyCode}) => {
-    switch (keyCode) {
-        case 65:
-        case 37:
-            console.log('left')
-            player.velocity.x = 0
-            break
+// addEventListener('keyup', ({keyCode}) => {
+//     switch (keyCode) {
+//         case 65:
+//         case 37:
+//             console.log('left')
+//             player.velocity.x = 0
+//             break
          
-        case 83:
-        case 40:
-            console.log('down')
-            player.velocity.y = 0
-            break
-        case 68:
-        case 39:
-            console.log('right')
-            player.velocity.x = 0
-            break   
-        case 87:
-        case 38:
-            console.log('up')
-            player.velocity.y = 0
-            break
-    }
-})
+//         case 83:
+//         case 40:
+//             console.log('down')
+//             player.velocity.y = 0
+//             break
+//         case 68:
+//         case 39:
+//             console.log('right')
+//             player.velocity.x = 0
+//             break   
+//         case 87:
+//         case 38:
+//             console.log('up')
+//             player.velocity.y = 0
+//             break
+//     }
+// })
 
-let frameCount = 1;
-let obsCount = frameCount;
-const obsXCoors = [];
+// // obstacle creation 
+// class Obstacle {
+    
+// }
 
-const nextFrame = () => {
-    frameCount++;
-    for (let i = 0; i < obsCount; i++) {
-        obsXCoor = Math.floor(Math.random() * (1165 - 40 + 1) + 140);
-        obsXCoors.push(obsXCoor);
-    } 
-}
+// const obstacle = new Obstacle()
 
 
